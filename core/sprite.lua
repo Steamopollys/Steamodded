1c3886d0905f10363deb955ea7342671a35156ac
Steamo <steamo@gmail.com>
2024-02-23 03:59:30 +0100

    Alpha Sprite API

--- core/sprite.lua
+++ core/sprite.lua
@@ -1,151 +1,165 @@
--- FULLY DISABLED BEFORE REWORK !
 -- ----------------------------------------------
 -- ------------MOD CORE API SPRITE---------------
 
 
--- -- Original Code by MathIsFun_
--- -- A proper ressources handler will be created later
--- SMODS.Sprites = {}
--- SMODS.Sprite = {name = "", px = "", py = "", path = ""}
+-- BASE REFERENCES FROM MAIN GAME
+-- G.animation_atli = {
+--     {name = "blind_chips", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/BlindChips.png",px=34,py=34, frames = 21},
+--     {name = "shop_sign", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/ShopSignAnimation.png",px=113,py=57, frames = 4}
+-- }
+-- G.asset_atli = {
+--     {name = "cards_1", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/8BitDeck.png",px=71,py=95},
+--     {name = "cards_2", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/8BitDeck_opt2.png",px=71,py=95},
+--     {name = "centers", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/Enhancers.png",px=71,py=95},
+--     {name = "Joker", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/Jokers.png",px=71,py=95},
+--     {name = "Tarot", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/Tarots.png",px=71,py=95},
+--     {name = "Voucher", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/Vouchers.png",px=71,py=95},
+--     {name = "Booster", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/boosters.png",px=71,py=95},
+--     {name = "ui_1", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/ui_assets.png",px=18,py=18},
+--     {name = "ui_2", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/ui_assets_opt2.png",px=18,py=18},
+--     {name = "balatro", path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/balatro.png",px=333,py=216},        
+--     {name = 'gamepad_ui', path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/gamepad_ui.png",px=32,py=32},
+--     {name = 'icons', path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/icons.png",px=66,py=66},
+--     {name = 'tags', path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/tags.png",px=34,py=34},
+--     {name = 'stickers', path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/stickers.png",px=71,py=95},
+--     {name = 'chips', path = "resources/textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/chips.png",px=29,py=29}
+-- }
+-- G.asset_images = {
+--     {name = "playstack_logo", path = "resources/textures/1x/playstack-logo.png", px=1417,py=1417},
+--     {name = "localthunk_logo", path = "resources/textures/1x/localthunk-logo.png", px=1390,py=560}
+-- }
 
--- function SMODS.Sprite:new(name, px, py, path)
--- 	o = {}
--- 	setmetatable(o, self)
--- 	self.__index = self
+SMODS.Sprites = {}
+SMODS.Sprite = {name = "", top_lpath = "", path = "", px = 0, py = 0, type = "", frames = 0}
 
--- 	o.name = name
--- 	o.px = px
--- 	o.py = py
--- 	o.path = path
+function SMODS.Sprite:new(name, top_lpath, path, px, py, type, frames)
+	o = {}
+	setmetatable(o, self)
+	self.__index = self
 
--- 	return o
--- end
+	o.name = name
+    o.top_lpath = top_lpath .. "assets/"
+	o.path = path
+	o.px = px
+	o.py = py
+    if type == "animation_atli" then
+        o.frames = frames
+        o.type = type
+    elseif type == "asset_atli" or type == "asset_images" then
+        o.type = type
+    else
+        error("Bad Sprite type")
+    end
 
--- function SMODS.Sprite:register()
--- 	if not SMODS.Sprites[self] then
--- 		table.insert(SMODS.Sprites, self)
--- 	end
--- end
+	return o
+end
 
--- -- Inspired by the Code of MathIsFun_
--- function SMODS.injectSprites()
--- 	for i, sprite in ipairs(SMODS.Sprites) do
--- 		G.ASSET_ATLAS[sprite.name] = {}
--- 		G.ASSET_ATLAS[sprite.name].name = sprite.name
--- 		G.ASSET_ATLAS[sprite.name].image = love.graphics.newImage(sprite.path, {
--- 			mipmaps = true,
--- 			dpiscale = G.SETTINGS.GRAPHICS.texture_scaling
--- 		})
--- 		G.ASSET_ATLAS[sprite.name].type = sprite.type
--- 		G.ASSET_ATLAS[sprite.name].px = sprite.px
--- 		G.ASSET_ATLAS[sprite.name].py = sprite.py
+function SMODS.Sprite:register()
+	if not SMODS.Sprites[self] then
+		table.insert(SMODS.Sprites, self)
+	end
+end
 
--- 		sendDebugMessage("The Sprite named " .. sprite.name .. " have been registered.")
--- 	end
--- end
+function SMODS.injectSprites()
 
--- -- Original Code by MathIsFun_
--- -- It replace the game function, will be made modular later 
--- function Card.set_sprites(arg_865_0, arg_865_1, arg_865_2)
--- 	if arg_865_2 then
--- 		if arg_865_0.children.front then
--- 			arg_865_0.children.front.atlas = G.ASSET_ATLAS[arg_865_2.atlas] or G.ASSET_ATLAS.cards
-			
--- 			arg_865_0.children.front:set_sprite_pos(arg_865_0.config.card.pos)
--- 		else
--- 			if arg_865_0.config.card.pos then
--- 				if arg_865_0.config.card.pos.atlas then
--- 					arg_865_0.children.front = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, arg_865_2.atlas and G.ASSET_ATLAS[arg_865_0.config.card.pos.atlas], arg_865_0.config.card.pos)
--- 				end
--- 			end
--- 			if not arg_865_0.children.front then
--- 				arg_865_0.children.front = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, arg_865_2.atlas and G.ASSET_ATLAS[arg_865_2.atlas] or G.ASSET_ATLAS.cards, arg_865_0.config.card.pos)
--- 			end
--- 			arg_865_0.children.front.states.hover = arg_865_0.states.hover
--- 			arg_865_0.children.front.states.click = arg_865_0.states.click
--- 			arg_865_0.children.front.states.drag = arg_865_0.states.drag
--- 			arg_865_0.children.front.states.collide.can = false
-			
--- 			arg_865_0.children.front:set_role({
--- 				role_type = "Minor",
--- 				major = arg_865_0,
--- 				draw_major = arg_865_0
--- 			})
--- 		end
--- 	end
-	
--- 	if arg_865_1 then
--- 		if arg_865_1.set then
--- 			if arg_865_0.children.center then
--- 				if arg_865_0.config.center.pos then
--- 					if arg_865_0.config.center.pos.atlas then
--- 						arg_865_0.children.center.atlas = G.ASSET_ATLAS[arg_865_0.config.center.pos.atlas]
--- 					end
--- 				else
--- 					arg_865_0.children.center.atlas = G.ASSET_ATLAS[arg_865_1.atlas or not (arg_865_1.set ~= "Joker" and not arg_865_1.consumeable and arg_865_1.set ~= "Voucher") and arg_865_1.set or "centers"]
--- 				end
--- 				arg_865_0.children.center:set_sprite_pos(arg_865_0.config.center.pos)
--- 			else
--- 				if arg_865_0.config.center.pos then
--- 					if arg_865_0.config.center.pos.atlas then
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS[arg_865_0.config.center.pos.atlas], arg_865_0.config.center.pos)
--- 					end
--- 				end
--- 				if not arg_865_0.children.center then
--- 					if arg_865_1.set == "Joker" and not arg_865_1.unlocked and not arg_865_0.params.bypass_discovery_center then
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS.Joker, G.j_locked.pos)
--- 					elseif arg_865_0.config.center.set == "Voucher" and not arg_865_0.config.center.unlocked and not arg_865_0.params.bypass_discovery_center then
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS.Voucher, G.v_locked.pos)
--- 					elseif arg_865_0.config.center.consumeable and arg_865_0.config.center.demo then
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS.Tarot, G.c_locked.pos)
--- 					elseif not arg_865_0.params.bypass_discovery_center and (arg_865_1.set == "Edition" or arg_865_1.set == "Joker" or arg_865_1.consumeable or arg_865_1.set == "Voucher" or arg_865_1.set == "Booster") and not arg_865_1.discovered then
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS[arg_865_1.atlas or arg_865_1.set], arg_865_1.set == "Joker" and G.j_undiscovered.pos or arg_865_1.set == "Edition" and G.j_undiscovered.pos or arg_865_1.set == "Tarot" and G.t_undiscovered.pos or arg_865_1.set == "Planet" and G.p_undiscovered.pos or arg_865_1.set == "Spectral" and G.s_undiscovered.pos or arg_865_1.set == "Voucher" and G.v_undiscovered.pos or arg_865_1.set == "Booster" and G.booster_undiscovered.pos)
--- 					elseif arg_865_1.set == "Joker" or arg_865_1.consumeable or arg_865_1.set == "Voucher" then
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS[arg_865_1.set], arg_865_0.config.center.pos)
--- 					else
--- 						arg_865_0.children.center = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS[arg_865_1.atlas or "centers"], arg_865_0.config.center.pos)
--- 					end
--- 				end
-				
--- 				arg_865_0.children.center.states.hover = arg_865_0.states.hover
--- 				arg_865_0.children.center.states.click = arg_865_0.states.click
--- 				arg_865_0.children.center.states.drag = arg_865_0.states.drag
--- 				arg_865_0.children.center.states.collide.can = false
-				
--- 				arg_865_0.children.center:set_role({
--- 					role_type = "Minor",
--- 					major = arg_865_0,
--- 					draw_major = arg_865_0
--- 				})
--- 			end
-			
--- 			if arg_865_1.name == "Half Joker" and (arg_865_1.discovered or arg_865_0.bypass_discovery_center) then
--- 				arg_865_0.children.center.scale.y = arg_865_0.children.center.scale.y / 1.7
--- 			end
--- 		end
+	for i, sprite in ipairs(SMODS.Sprites) do
+        local foundAndReplaced = false
+
+		if sprite.type == "animation_atli" then
+            for i, asset in ipairs(G.animation_atli) do
+                if asset.name == sprite.name then
+                    G.animation_atli[i] = {name = sprite.name, path = sprite.top_lpath .. G.SETTINGS.GRAPHICS.texture_scaling .. 'x/' .. sprite.path, px = sprite.px, py = sprite.py , frames = sprite.frames}
+                    foundAndReplaced = true
+                    break
+                end
+            end
+            if foundAndReplaced ~= true then
+                table.insert(G.animation_atli, {name = sprite.name, path = sprite.top_lpath .. G.SETTINGS.GRAPHICS.texture_scaling .. 'x/' .. sprite.path, px = sprite.px, py = sprite.py , frames = sprite.frames})
+            end
+        elseif sprite.type == "asset_atli" then
+            for i, asset in ipairs(G.asset_atli) do
+                if asset.name == sprite.name then
+                    sendDebugMessage(inspectDepth(G.asset_atli[i]))
+                    sendDebugMessage(inspectDepth({name = sprite.name, path = sprite.top_lpath .. G.SETTINGS.GRAPHICS.texture_scaling .. 'x/' .. sprite.path, px = sprite.px, py = sprite.py}))
+                    G.asset_atli[i] = {name = sprite.name, path = sprite.top_lpath .. G.SETTINGS.GRAPHICS.texture_scaling .. 'x/' .. sprite.path, px = sprite.px, py = sprite.py}
+                    foundAndReplaced = true
+                    break
+                end
+            end
+            if foundAndReplaced ~= true then
+                table.insert(G.asset_atli, {name = sprite.name, path = sprite.top_lpath .. G.SETTINGS.GRAPHICS.texture_scaling .. 'x/' .. sprite.path, px = sprite.px, py = sprite.py})
+            end
+        elseif sprite.type == "asset_images" then
+            for i, asset in ipairs(G.asset_images) do
+                if asset.name == sprite.name then
+                    G.asset_images[i] = {name = sprite.name, path = sprite.top_lpath .. '1x/' .. sprite.path, px = sprite.px, py = sprite.py}
+                    foundAndReplaced = true
+                    break
+                end
+            end
+            if foundAndReplaced ~= true then
+                table.insert(G.asset_images, {name = sprite.name, path = sprite.top_lpath .. '1x/' .. sprite.path, px = sprite.px, py = sprite.py})
+            end
+        else
+            error("Bad Sprite type")
+        end
 		
--- 		if arg_865_1.soul_pos then
--- 			arg_865_0.children.floating_sprite = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS.Joker, arg_865_0.config.center.soul_pos)
--- 			arg_865_0.children.floating_sprite.role.draw_major = arg_865_0
--- 			arg_865_0.children.floating_sprite.states.hover.can = false
--- 			arg_865_0.children.floating_sprite.states.click.can = false
--- 		end
-		
--- 		if not arg_865_0.children.back then
--- 			arg_865_0.children.back = Sprite(arg_865_0.T.x, arg_865_0.T.y, arg_865_0.T.w, arg_865_0.T.h, G.ASSET_ATLAS.centers, arg_865_0.params.bypass_back or arg_865_0.playing_card and G.GAME[arg_865_0.back].pos or G.P_CENTERS.b_blue.pos)
--- 			arg_865_0.children.back.states.hover = arg_865_0.states.hover
--- 			arg_865_0.children.back.states.click = arg_865_0.states.click
--- 			arg_865_0.children.back.states.drag = arg_865_0.states.drag
--- 			arg_865_0.children.back.states.collide.can = false
-			
--- 			arg_865_0.children.back:set_role({
--- 				role_type = "Minor",
--- 				major = arg_865_0,
--- 				draw_major = arg_865_0
--- 			})
--- 		end
--- 	end
--- end
+		sendDebugMessage("The Sprite named " .. sprite.name .. " with path " .. sprite.path .. " have been registered.")
+	end
+
+    --Reload Textures
+    
+    G.SETTINGS.GRAPHICS.texture_scaling = G.SETTINGS.GRAPHICS.texture_scaling or 2
+
+    --Set fiter to linear interpolation and nearest, best for pixel art
+    love.graphics.setDefaultFilter(
+        G.SETTINGS.GRAPHICS.texture_scaling == 1 and 'nearest' or 'linear',
+        G.SETTINGS.GRAPHICS.texture_scaling == 1 and 'nearest' or 'linear', 1)
+
+    --self.CANVAS = self.CANVAS or love.graphics.newCanvas(500, 500, {readable = true})
+    love.graphics.setLineStyle("rough")
+
+    for i=1, #G.animation_atli do
+        G.ANIMATION_ATLAS[G.animation_atli[i].name] = {}
+        G.ANIMATION_ATLAS[G.animation_atli[i].name].name = G.animation_atli[i].name
+        G.ANIMATION_ATLAS[G.animation_atli[i].name].image = love.graphics.newImage(G.animation_atli[i].path, {mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling})
+        G.ANIMATION_ATLAS[G.animation_atli[i].name].px = G.animation_atli[i].px
+        G.ANIMATION_ATLAS[G.animation_atli[i].name].py = G.animation_atli[i].py
+        G.ANIMATION_ATLAS[G.animation_atli[i].name].frames = G.animation_atli[i].frames
+    end
+
+    for i=1, #G.asset_atli do
+        G.ASSET_ATLAS[G.asset_atli[i].name] = {}
+        G.ASSET_ATLAS[G.asset_atli[i].name].name = G.asset_atli[i].name
+        G.ASSET_ATLAS[G.asset_atli[i].name].image = love.graphics.newImage(G.asset_atli[i].path, {mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling})
+        G.ASSET_ATLAS[G.asset_atli[i].name].type = G.asset_atli[i].type
+        G.ASSET_ATLAS[G.asset_atli[i].name].px = G.asset_atli[i].px
+        G.ASSET_ATLAS[G.asset_atli[i].name].py = G.asset_atli[i].py
+    end
+    for i=1, #G.asset_images do
+        G.ASSET_ATLAS[G.asset_images[i].name] = {}
+        G.ASSET_ATLAS[G.asset_images[i].name].name = G.asset_images[i].name
+        G.ASSET_ATLAS[G.asset_images[i].name].image = love.graphics.newImage(G.asset_images[i].path, {mipmaps = true, dpiscale = 1})
+        G.ASSET_ATLAS[G.asset_images[i].name].type = G.asset_images[i].type
+        G.ASSET_ATLAS[G.asset_images[i].name].px = G.asset_images[i].px
+        G.ASSET_ATLAS[G.asset_images[i].name].py = G.asset_images[i].py
+    end
+
+    for _, v in pairs(G.I.SPRITE) do
+        v:reset()
+    end
+
+    G.ASSET_ATLAS.Planet = G.ASSET_ATLAS.Tarot
+    G.ASSET_ATLAS.Spectral = G.ASSET_ATLAS.Tarot
+
+    sendDebugMessage("All the sprites have been loaded!")
+end
+
+gameset_render_settingsRef = Game.set_render_settings
+function Game:set_render_settings()
+    gameset_render_settingsRef(self)
+    SMODS.injectSprites()
+end
 
 -- ----------------------------------------------
 -- ------------MOD CORE API SPRITE END-----------
---
+++ example_mods/NegateTexturePack/NegateTexturePack.lua
@@ -0,0 +1,24 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Negate Texture Pack
+--- MOD_ID: NegateTexturePack
+--- MOD_AUTHOR: [Steamo]
+--- MOD_DESCRIPTION: Negated Balatro... THIS IS AN EXAMPLE MOD, FEEL FREE TO USE IT AS A BASE
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.NegateTexturePack()
+    sendDebugMessage("Launching Negate Texture Pack!")
+
+    local negate_mod = SMODS.findModByID("NegateTexturePack")
+    local sprite_jkr = SMODS.Sprite:new("Joker", negate_mod.path, "Jokers-negate.png", 71, 95, "asset_atli")
+    local sprite_boost = SMODS.Sprite:new("Booster", negate_mod.path, "boosters-negate.png", 71, 95, "asset_atli")
+    local sprite_blind = SMODS.Sprite:new("blind_chips", negate_mod.path, "BlindChips-negate.png", 34, 34, "animation_atli", 21)
+
+    sprite_jkr:register()
+    sprite_boost:register()
+    sprite_blind:register()
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
