6da6b73619675c02d789aef73d305ecf4360724f
Infarcactus <149241321+Infarcactus@users.noreply.github.com>
2024-04-17 10:31:55 +0200

    Update sound.lua
    Less processing to find a sound_code
    Changed the way to load sounds to just add a way to load all sounds just from the modId

--- core/sound.lua
+++ core/sound.lua
@@ -1,13 +1,29 @@
 SMODS.SOUND_SOURCES = {}
 
-function register_sound(name, path, filename)
+function Add_Custom_Sound_Global(modID)
+    local mod = SMODS.findModByID(modID)
+    for _, filename in ipairs(love.filesystem.getDirectoryItems(mod.path .. 'Assets')) do
+        local extension = string.sub(filename, -4)
+        if extension == '.ogg' or extension == '.mp3' or extension=='.wav' then --please use .ogg or .wav files
+            local sound = nil
+            local sound_code = string.sub(filename, 1, -5)
+            sound = {sound = love.audio.newSource(mod.path .. 'assets/sounds/' .. filename, 'static')}
+            sound.sound_code = sound_code
+            SMODS.SOUND_SOURCES[sound_code]=sound
+	    sendInfoMessage("Registered sound " .. name .. " from file " .. filename, 'SoundAPI')
+        end
+    end
+end
+
+
+function register_sound(name, path, filename) --Keep that here to support old versions
 	local sound_code = string.sub(filename, 1, -5)
 	local s = {
-		sound = love.audio.newSource(path .. "assets/sounds/" .. filename, string.find(sound_code,'music') and "stream" or 'static'),
+		sound = love.audio.newSource(path .. "assets/sounds/" .. filename, (string.find(sound_code,'music') or string.find(sound_code,'stream') and "stream" or 'static'),
 		filepath = path .. "assets/sounds/" .. filename
 	}
-	s.original_pitch = 1
-	s.original_volume = 0.75
+	--s.original_pitch = 1
+	--s.original_volume = 0.75
 	s.sound_code = name
 
 	sendInfoMessage("Registered sound " .. name .. " from file " .. filename, 'SoundAPI')
@@ -15,36 +31,24 @@ function register_sound(name, path, filename)
 end
 
 
-function modded_play_sound(sound_code, stop_previous_instance, volume, pitch)
-    stop_previous_instance = stop_previous_instance or false
-    sound_code = string.lower(sound_code)
-    for _, s in pairs(SMODS.SOUND_SOURCES) do
-        if s.sound_code == sound_code then
-            if volume then
-                s.original_volume = volume
-            else
-                s.original_volume = 1
-            end
-            if pitch then
-                s.original_pitch = pitch
-            else
-                s.original_pitch = 1
-            end
-            sendTraceMessage("found sound code: " .. sound_code, 'SoundAPI')
-            s.sound:setPitch(pitch)
-            local sound_vol = s.original_volume*(G.SETTINGS.SOUND.volume/100.0)*(G.SETTINGS.SOUND.game_sounds_volume/100.0)
-            if sound_vol <= 0 then
-                s.sound:setVolume(0)
-            else
-                s.sound:setVolume(sound_vol)
-            end
-            s.sound:setPitch(s.original_pitch)
-            if stop_previous_instance and s.sound:isPlaying() then
-                s.sound:stop()
-            end
-            love.audio.play(s.sound)
-            return true
+function Custom_Play_Sound(sound_code,stop_previous_instance, volume, pitch)
+    if SMODS.SOUND_SOURCES[sound_code] then
+	sendTraceMessage("found sound code: " .. sound_code, 'SoundAPI')
+        local s = SMODS.SOUND_SOURCES[sound_code]
+        stop_previous_instance = (stop_previous_instance == nil) and true or stop_previous_instance
+        volume = volume or 1
+        s.sound:setPitch(pitch or 1)
+        local sound_vol = volume*(G.SETTINGS.SOUND.volume/100.0)*(G.SETTINGS.SOUND.game_sounds_volume/100.0)
+        if sound_vol <= 0 then
+            s.sound:setVolume(0)
+        else
+            s.sound:setVolume(sound_vol)
         end
+        if stop_previous_instance and s.sound:isPlaying() then
+            s.sound:stop()
+        end
+        love.audio.play(s.sound)
+        return true
     end
     return false
 end
