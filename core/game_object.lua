fcf379176363c45aaead34d7e9f0216272cfe160
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-05-08 00:03:21 -0400

    custom sprites and support for lots of stakes

--- core/game_object.lua
+++ core/game_object.lua
@@ -1630,6 +1630,7 @@ function loadAPIs()
         prefix = 'stake',
         unlocked = false,
         set = 'Stake',
+        atlas = 'chips',
         required_params = {
             'name',
             'pos',
@@ -1705,31 +1706,50 @@ function loadAPIs()
     end
     end
 
-    function SMODS.applied_stakes_UI(i, stake_desc_rows)
+    function SMODS.applied_stakes_UI(i, stake_desc_rows, num_added)
+    if num_added == nil then num_added = {val = 0} end
     if G.P_CENTER_POOLS['Stake'][i].applied_stakes then
         for _, v in pairs(G.P_CENTER_POOLS['Stake'][i].applied_stakes) do
         if v ~= "white" then
-            local i = G.P_STAKES["stake_"..v].stake_level
-            local _stake_desc = {}
-            local _stake_center = G.P_CENTER_POOLS.Stake[i]
-            localize{type = 'descriptions', key = _stake_center.key, set = _stake_center.set, nodes = _stake_desc}
-            local _full_desc = {}
-            for k, v in ipairs(_stake_desc) do
-            _full_desc[#_full_desc+1] = {n=G.UIT.R, config={align = "cm"}, nodes=v}
+            --todo: manage this with pages
+            if num_added.val < 8 then
+                local i = G.P_STAKES["stake_"..v].stake_level
+                local _stake_desc = {}
+                local _stake_center = G.P_CENTER_POOLS.Stake[i]
+                localize{type = 'descriptions', key = _stake_center.key, set = _stake_center.set, nodes = _stake_desc}
+                local _full_desc = {}
+                for k, v in ipairs(_stake_desc) do
+                _full_desc[#_full_desc+1] = {n=G.UIT.R, config={align = "cm"}, nodes=v}
+                end
+                _full_desc[#_full_desc] = nil
+                stake_desc_rows[#stake_desc_rows+1] = {n=G.UIT.R, config={align = "cm"}, nodes={
+                {n=G.UIT.C, config={align = 'cm'}, nodes ={{n=G.UIT.C, config={align = "cm", colour = get_stake_col(i), r = 0.1, minh = 0.35, minw = 0.35, emboss = 0.05}, nodes={}}, {n=G.UIT.B, config={w=0.1,h=0.1}}}},
+                {n=G.UIT.C, config={align = "cm", padding = 0.03, colour = G.C.WHITE, r = 0.1, minh = 0.7, minw = 4.8}, nodes=_full_desc},
+                }}
             end
-            _full_desc[#_full_desc] = nil
-            stake_desc_rows[#stake_desc_rows+1] = {n=G.UIT.R, config={align = "cm"}, nodes={
-            {n=G.UIT.C, config={align = 'cm'}, nodes ={{n=G.UIT.C, config={align = "cm", colour = get_stake_col(i), r = 0.1, minh = 0.35, minw = 0.35, emboss = 0.05}, nodes={}}, {n=G.UIT.B, config={w=0.1,h=0.1}}}},
-            {n=G.UIT.C, config={align = "cm", padding = 0.03, colour = G.C.WHITE, r = 0.1, minh = 0.7, minw = 4.8}, nodes=_full_desc},
-            }}
-            SMODS.applied_stakes_UI(G.P_STAKES["stake_"..v].stake_level, stake_desc_rows)
+            num_added.val = num_added.val + 1
+            num_added.val = SMODS.applied_stakes_UI(G.P_STAKES["stake_"..v].stake_level, stake_desc_rows, num_added)
         end
         end
     end
     end
 
-    -- Stickers
-    local startup = G.start_up
+    -- We're overwriting so much that it's better to just remake this
+    function G.UIDEF.deck_stake_column(_deck_key)
+        local deck_usage = G.PROFILES[G.SETTINGS.profile].deck_usage[_deck_key]
+        local stake_col = {}
+        local valid_option = nil
+        local num_stakes = #G.P_CENTER_POOLS['Stake']
+        for i = #G.P_CENTER_POOLS['Stake'], 1, -1 do
+        local _wins = deck_usage and deck_usage.wins[i] or 0
+        if (deck_usage and deck_usage.wins[i-1]) or i == 1 or G.PROFILES[G.SETTINGS.profile].all_unlocked then valid_option = true end
+        stake_col[#stake_col+1] = {n=G.UIT.R, config={id = i, align = "cm", colour = _wins > 0 and G.C.GREY or G.C.CLEAR, outline = 0, outline_colour = G.C.WHITE, r = 0.1, minh = 2/num_stakes, minw = valid_option and 0.45 or 0.25, func = 'RUN_SETUP_check_back_stake_highlight'}, nodes={
+            {n=G.UIT.R, config={align = "cm", minh = valid_option and 1.36/num_stakes or 1.04/num_stakes, minw = valid_option and 0.37 or 0.13, colour = _wins > 0 and get_stake_col(i) or G.C.UI.TRANSPARENT_LIGHT, r = 0.1},nodes={}}
+        }}
+        if i > 1 then stake_col[#stake_col+1] = {n=G.UIT.R, config={align = "cm", minh = 0.8/num_stakes, minw = 0.04},nodes={}} end
+        end
+        return {n=G.UIT.ROOT, config={align = 'cm', colour = G.C.CLEAR}, nodes =stake_col}
+    end
 
     --Register vanilla stakes
     G.P_CENTER_POOLS['Stake'] = {}
