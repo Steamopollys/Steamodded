ffd45a03121d1698aa272bacc83b0a413af8274b
english5040 <ejwu@andrew.cmu.edu>
2024-05-23 19:38:26 -0400

    Add files from Eremel's EnhanceAPI repo

---
+++ example_mods/Mods/OrtalabEditions/core/OrtalabEditions.lua
@@ -0,0 +1,356 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Ortalab Editions
+--- MOD_ID: OrtalabEditions
+--- MOD_AUTHOR: [Eremel_]
+--- MOD_DESCRIPTION: Adds the editions for the Ortalab mod.
+--- BADGE_COLOUR: 3FC7EB
+----------------------------------------------
+------------MOD CODE -------------------------
+
+-- disable if you like
+local test_deck_def = {
+    ["name"] = "Test Deck",
+    ["text"] = {
+        "Start with a {C:spectral}High Exposure{}",
+        "and a {C:spectral}Neon{}."
+    }
+}
+
+function SMODS.INIT.OrtalabEditions()
+    local mod = SMODS.findModByID("OrtalabEditions")
+    SMODS.Sprite:new("High Exposure", mod.path, "high exposure.png", 71, 95, "asset_atli"):register()
+    SMODS.Sprite:new("Neon", mod.path, "neon.png", 71, 95, "asset_atli"):register()
+    SMODS.Sprite:new("Grey", mod.path, "grey.png", 71, 95, "asset_atli"):register()
+
+    local fluorDef = {
+        name = "Fluorescent",
+        slug = "fluorescent",
+        config = { labels = {'mult_mod'}, values = {100} },
+        shader_path = mod.path .. "/assets/shaders/fluorescent.fs",
+        loc_txt = { name = "Fluorescent", text = {"{C:mult}+#1#{} Mult"}},
+        discovered = true,
+        unlocked = true,
+        unlock_condition = {},
+        badge_colour = G.C.EDITION,
+        extra_cost = 5,
+        calculate = function(self, context)
+            if context.edition or (context.cardarea == G.play and self.playing_card) then
+                ret = {}
+                for k, v in pairs(self.edition) do
+                    ret[k] = v
+                end
+                return ret
+            end
+        end
+    }
+
+    local overexposedDef = {
+        name = "Overexposed",
+        slug = "overexposed",
+        config = { labels = {'repetitions'}, values = {1} },
+        shader_path = mod.path .. "/assets/shaders/overexposed.fs",
+        loc_txt = { name = "Overexposed", text = {"{C:money}+$#1#{} when a hand","is scored"}},
+        discovered = true,
+        unlocked = true,
+        unlock_condition = {},
+        apply_to_float = true,
+        badge_colour = G.C.DARK_EDITION,
+        weight = 5,
+        in_shop = true,
+        extra_cost = 4,
+        calculate = function(self, context)
+            if context.repetition then
+                return {
+                    message = localize('k_again_ex'),
+                    repetitions = 1,
+                    card = self
+                }
+            end
+        end
+    }
+
+    local greyDef = {
+        name = "Greyscale",
+        slug = "greyscale",
+        config = { labels = {'chip_mod','mult_mod','x_mult_mod'}, values = {200, 10, 2} },
+        shader_path = mod.path .. "/assets/shaders/greyscale.fs",
+        loc_txt = { name = "Greyscale", text = {"{C:chips}+#1#{} chips, {C:mult}+#2#{} Mult", "and {X:mult,C:white}X#3#{} Mult"}},
+        discovered = true,
+        unlocked = true,
+        unlock_condition = {},
+        apply_to_float = true,
+        weight = 8,
+        in_shop = true,
+        extra_cost = 6,
+        calculate = function(self, context)
+            if context.edition or (context.cardarea == G.play and self.playing_card) then
+                ret = {}
+                for k, v in pairs(self.edition) do
+                    ret[k] = v
+                end
+                return ret
+            end
+        end
+    }
+
+    local anaglyphDef = {
+        name = "Anaglyphic",
+        slug = "anaglyphic",
+        config = { labels = {'chip_mod', 'mult_mod'}, values = {-50, 50} },
+        shader_path = mod.path .. "/assets/shaders/anaglyphic.fs",
+        loc_txt = { name = "Anaglyphic", text = {"{C:chips}#1#{} chips, {C:mult}+#2#{} Mult"}},
+        discovered = true,
+        unlocked = true,
+        unlock_condition = {},
+        badge_colour = G.C.DARK_EDITION,
+        weight = 10,
+        in_shop = true,
+        extra_cost = 1,
+        calculate = function(self, context)
+            if context.edition or (context.cardarea == G.play and self.playing_card) then
+                ret = {}
+                for k, v in pairs(self.edition) do
+                    ret[k] = v
+                end
+                return ret
+            end
+        end
+    }
+    
+    local flippedDef = {
+        name = "Monochrome",
+        slug = "monochrome",
+        config = { labels = {'chip_mod'}, values = {10} },
+        shader_path = mod.path .. "/assets/shaders/monochrome.fs",
+        loc_txt = { name = "Monochrome", text = {"Earn {C:money}$#1#{} when a","hand is scored"}},
+        discovered = true,
+        unlocked = true,
+        unlock_condition = {},
+        badge_colour = G.C.DARK_EDITION,
+        calculate = function(self, context)
+            if context.edition or (context.cardarea == G.play and self.playing_card) then
+                ease_dollars(self.edition.p_dollars)
+                card_eval_status_text(self, 'dollars', self.edition.p_dollars)
+            end
+        end
+    }
+
+    local sepiaDef = {
+        name = "Sepia",
+        slug = "sepia",
+        config = { labels = {'chip_mod'}, values = {10} },
+        shader_path = mod.path .. "/assets/shaders/sepia.fs",
+        loc_txt = { name = "Sepia", text = {"{C:chips}+#1#{} chips"}},
+        discovered = true,
+        unlocked = true,
+        unlock_condition = {},
+        badge_colour = G.C.DARK_EDITION,
+        weight = 3,
+        in_shop = true,
+        extra_cost = 4,
+        calculate = function(self, context)
+            if context.edition or (context.cardarea == G.play and self.playing_card) then
+                hand_chips = mod_chips(hand_chips + self.edition.chip_mod)
+                update_hand_text({delay = 0}, {chips = hand_chips})
+                card_eval_status_text(self, 'chips', self.edition. chip_mod, percent)
+                -- ret = {}
+                -- for k, v in pairs(self.edition) do
+                --     ret[k] = v
+                -- end
+                -- return ret
+            end
+        end
+    }
+
+    SMODS.Edition:new(greyDef):register()
+    SMODS.Edition:new(anaglyphDef):register()
+    SMODS.Edition:new(overexposedDef):register()
+    SMODS.Edition:new(fluorDef):register()
+    SMODS.Edition:new(flippedDef):register()
+    SMODS.Edition:new(sepiaDef):register()    
+        
+    local c_high_exposure = SMODS.Spectral:new('High Exposure', 'high_exposure', {}, {
+        x = 0,
+        y = 0
+    }, {
+        name = "Test Card",
+        text = {"Add {C:dark_edition}Foil{} or {C:dark_edition}Sepia{}","to a random joker"}
+    }, 4, nil, nil, 'High Exposure')
+
+    local c_neon = SMODS.Spectral:new('Neon', 'neon', {}, {
+        x = 0,
+        y = 0
+    }, {
+        name = "+Sepia",
+        text = {"Increase {C:dark_edition}Sepia{} chip", "value by {C:chips}+10{}"}
+    }, 4, nil, nil, 'Neon')
+
+    local c_grey = SMODS.Spectral:new('Grey', 'grey', {}, {
+        x = 0,
+        y = 0
+    }, {
+        name = "Grey",
+        text = {"Add {C:dark_edition}Greyscale{}", "to a random joker"}
+    }, 4, nil, nil, 'Grey')
+
+    c_high_exposure:register()
+    c_neon:register()
+    c_grey:register()
+
+    
+    -- two cards in hand overexposed
+    function SMODS.Spectrals.c_high_exposure.can_use(card)
+        if G.STATE ~= G.STATES.HAND_PLAYED and G.STATE ~= G.STATES.DRAW_TO_HAND and G.STATE ~= G.STATES.PLAY_TAROT or
+            any_state then
+                if G.hand and (#G.hand.highlighted == 4) and G.hand.highlighted[1] and G.hand.highlighted[2] and G.hand.highlighted[3] and G.hand.highlighted[4] and (not G.hand.highlighted[1].edition) and (not G.hand.highlighted[2].edition) and (not G.hand.highlighted[3].edition) and (not G.hand.highlighted[4].edition) then return true end
+        end
+    end
+
+    function SMODS.Spectrals.c_high_exposure.use(card, area, copier)
+        local used_tarot = (copier or card)
+        local selected_cards = G.hand.highlighted
+        for _,_card in ipairs(selected_cards) do
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.6,
+            func = function()
+                    local selected_edition = poll_edition("custom_editions", nil, nil, true, {{name = "sepia", weight = 1}, {name = "overexposed", weight = 1}, {name = "anaglyphic", weight = 1}, {name = "fluorescent", weight = 1}})
+                    _card.set_edition(_card, selected_edition)
+                    for k,_ in pairs(selected_edition) do
+                        used_tarot:juice_up(0.3, 0.5)
+                    attention_text({
+                        text = "+ "..k,
+                        scale = 1, 
+                        hold = 0.4,
+                        major = used_tarot,
+                        backdrop_colour = G.C.UI_CHIPS,
+                        align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and 'tm' or 'cm',
+                        offset = {x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and -0.2 or 0},
+                        silent = true
+                        })
+                    end
+                    return true
+                end
+            }))
+        end
+    end
+
+    -- -- Set joker overexposed
+    -- function SMODS.Spectrals.c_high_exposure.can_use(card)
+    --     if G.STATE ~= G.STATES.HAND_PLAYED and G.STATE ~= G.STATES.DRAW_TO_HAND and G.STATE ~= G.STATES.PLAY_TAROT or
+    --         any_state then
+            
+    --         if next(SMODS.Edition:get_editionless_jokers({})) then
+    --             return true
+    --         end
+    --     end
+    -- end
+
+    -- function SMODS.Spectrals.c_high_exposure.use(card, area, copier)
+    --     local used_tarot = (copier or card)
+    --     local selected_cards = G.hand.highlighted
+    --     local eligible_jokers = SMODS.Edition:get_editionless_jokers({})
+    --     G.E_MANAGER:add_event(Event({
+    --         trigger = 'after',
+    --         delay = 0.4,
+    --         func = function()
+    --             local selected_joker = pseudorandom_element(eligible_jokers, pseudoseed('seed'))
+    --             local selected_edition = poll_edition("photograph", nil, nil, true, {{name = "foil", weight = 1}, {name = "polychrome", weight = 1}})
+    --             selected_joker.set_edition(selected_joker, { overexposed = true })
+    --             used_tarot:juice_up(0.3, 0.5)
+    --             return true
+    --         end
+    --     }))
+    -- end
+
+    function SMODS.Spectrals.c_high_exposure.loc_def(self, info_queue)
+        info_queue[#info_queue+1] = G.P_CENTERS.e_foil
+        info_queue[#info_queue+1] = G.P_CENTERS.e_sepia
+        return {}
+     end
+
+    function SMODS.Spectrals.c_neon.can_use(card)
+        if G.STATE ~= G.STATES.HAND_PLAYED and G.STATE ~= G.STATES.DRAW_TO_HAND and G.STATE ~= G.STATES.PLAY_TAROT or
+            any_state then
+            -- if next(SMODS.Edition:get_eligible_jokers({})) then
+            --     return true
+            -- end
+            return true
+        end
+    end
+
+    function SMODS.Spectrals.c_neon.use(card, area, copier)
+        local used_tarot = (copier or card)
+        local eligible_jokers = SMODS.Edition:get_editionless_jokers({})
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                local over = false
+                -- local selected_joker = pseudorandom_element(eligible_jokers, pseudoseed('seed'))
+                -- selected_joker.set_edition(selected_joker, { fluorescent = true })
+                SMODS.Editions.sepia:change_modifier("chip_mod", 10)
+                attention_text({
+                    text = "+10 chips",
+                    scale = 1, 
+                    hold = 1.4,
+                    major = used_tarot,
+                    backdrop_colour = G.C.UI_CHIPS,
+                    align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and 'tm' or 'cm',
+                    offset = {x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and -0.2 or 0},
+                    silent = true
+                    })
+                used_tarot:juice_up(0.3, 0.5)
+                return true
+            end
+        }))
+    end
+
+    function SMODS.Spectrals.c_neon.loc_def(self, info_queue)
+        info_queue[#info_queue+1] = G.P_CENTERS.e_sepia
+        return {}
+     end
+
+    function SMODS.Spectrals.c_grey.can_use(card)
+        if G.STATE ~= G.STATES.HAND_PLAYED and G.STATE ~= G.STATES.DRAW_TO_HAND and G.STATE ~= G.STATES.PLAY_TAROT or
+            any_state then
+            if next(SMODS.Edition:get_editionless_jokers({})) then
+                return true
+            end
+        end
+    end
+
+    function SMODS.Spectrals.c_grey.use(card, area, copier)
+        local used_tarot = (copier or card)
+        local eligible_jokers = SMODS.Edition:get_editionless_jokers({})
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                local over = false
+                local selected_joker = pseudorandom_element(eligible_jokers, pseudoseed('seed'))
+                selected_joker.set_edition(selected_joker, { sepia = true })
+                used_tarot:juice_up(0.3, 0.5)
+                return true
+            end
+        }))
+    end
+
+    function SMODS.Spectrals.c_grey.loc_def(self, info_queue)
+        info_queue[#info_queue+1] = G.P_CENTERS.e_greyscale
+        return {}
+     end
+
+    SMODS.Deck:new("Test Deck", "test_deck", {
+        consumables = {'c_high_exposure', 'c_neon'}
+    }, {
+        x = 0,
+        y = 0
+    }, test_deck_def):register()
+
+
+end
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
---
+++ example_mods/Mods/OrtalabEditions/core/assets/shaders/foil.fs
@@ -0,0 +1,143 @@
+#if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
+	#define MY_HIGHP_OR_MEDIUMP highp
+#else
+	#define MY_HIGHP_OR_MEDIUMP mediump
+#endif
+
+
+extern MY_HIGHP_OR_MEDIUMP vec2 foil;
+extern MY_HIGHP_OR_MEDIUMP number dissolve;
+extern MY_HIGHP_OR_MEDIUMP number time;
+extern MY_HIGHP_OR_MEDIUMP vec4 texture_details;
+extern MY_HIGHP_OR_MEDIUMP vec2 image_details;
+extern bool shadow;
+extern MY_HIGHP_OR_MEDIUMP vec4 burn_colour_1;
+extern MY_HIGHP_OR_MEDIUMP vec4 burn_colour_2;
+
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv)
+{
+    if (dissolve < 0.001) {
+        return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, shadow ? tex.a*0.3: tex.a);
+    }
+
+    float adjusted_dissolve = (dissolve*dissolve*(3.-2.*dissolve))*1.02 - 0.01; //Adjusting 0.0-1.0 to fall to -0.1 - 1.1 scale so the mask does not pause at extreme values
+
+	float t = time * 10.0 + 2003.;
+	vec2 floored_uv = (floor((uv*texture_details.ba)))/max(texture_details.b, texture_details.a);
+    vec2 uv_scaled_centered = (floored_uv - 0.5) * 2.3 * max(texture_details.b, texture_details.a);
+	
+	vec2 field_part1 = uv_scaled_centered + 50.*vec2(sin(-t / 143.6340), cos(-t / 99.4324));
+	vec2 field_part2 = uv_scaled_centered + 50.*vec2(cos( t / 53.1532),  cos( t / 61.4532));
+	vec2 field_part3 = uv_scaled_centered + 50.*vec2(sin(-t / 87.53218), sin(-t / 49.0000));
+
+    float field = (1.+ (
+        cos(length(field_part1) / 19.483) + sin(length(field_part2) / 33.155) * cos(field_part2.y / 15.73) +
+        cos(length(field_part3) / 27.193) * sin(field_part3.x / 21.92) ))/2.;
+    vec2 borders = vec2(0.2, 0.8);
+
+    float res = (.5 + .5* cos( (adjusted_dissolve) / 82.612 + ( field + -.5 ) *3.14))
+    - (floored_uv.x > borders.y ? (floored_uv.x - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y > borders.y ? (floored_uv.y - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.x < borders.x ? (borders.x - floored_uv.x)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y < borders.x ? (borders.x - floored_uv.y)*(5. + 5.*dissolve) : 0.)*(dissolve);
+
+    if (tex.a > 0.01 && burn_colour_1.a > 0.01 && !shadow && res < adjusted_dissolve + 0.8*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+        if (!shadow && res < adjusted_dissolve + 0.5*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+            tex.rgba = burn_colour_1.rgba;
+        } else if (burn_colour_2.a > 0.01) {
+            tex.rgba = burn_colour_2.rgba;
+        }
+    }
+
+    return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, res > adjusted_dissolve ? (shadow ? tex.a*0.3: tex.a) : .0);
+}
+
+number hue(number s, number t, number h)
+{
+	number hs = mod(h, 1.)*6.;
+	if (hs < 1.) return (t-s) * hs + s;
+	if (hs < 3.) return t;
+	if (hs < 4.) return (t-s) * (4.-hs) + s;
+	return s;
+}
+
+vec4 RGB(vec4 c)
+{
+	if (c.y < 0.0001)
+		return vec4(vec3(c.z), c.a);
+
+	number t = (c.z < .5) ? c.y*c.z + c.z : -c.y*c.z + (c.y+c.z);
+	number s = 2.0 * c.z - t;
+	return vec4(hue(s,t,c.x + 1./3.), hue(s,t,c.x), hue(s,t,c.x - 1./3.), c.w);
+}
+
+vec4 HSL(vec4 c)
+{
+	number low = min(c.r, min(c.g, c.b));
+	number high = max(c.r, max(c.g, c.b));
+	number delta = high - low;
+	number sum = high+low;
+
+	vec4 hsl = vec4(.0, .0, .5 * sum, c.a);
+	if (delta == .0)
+		return hsl;
+
+	hsl.y = (hsl.z < .5) ? delta / sum : delta / (2.0 - sum);
+
+	if (high == c.r)
+		hsl.x = (c.g - c.b) / delta;
+	else if (high == c.g)
+		hsl.x = (c.b - c.r) / delta + 2.0;
+	else
+		hsl.x = (c.r - c.g) / delta + 4.0;
+
+	hsl.x = mod(hsl.x / 6., 1.);
+	return hsl;
+}
+
+vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords )
+{
+    vec4 tex = Texel( texture, texture_coords);
+    vec2 uv = (((texture_coords)*(image_details)) - texture_details.xy*texture_details.ba)/texture_details.ba;
+    vec2 adjusted_uv = uv - vec2(0.5, 0.5);
+    adjusted_uv.x = adjusted_uv.x*texture_details.b/texture_details.a;
+
+    number low = min(tex.r, min(tex.g, tex.b));
+    number high = max(tex.r, max(tex.g, tex.b));
+	number delta = min(high, max(0.5, 1. - low));
+
+    number fac = max(min(2.*sin((length(90.*adjusted_uv) + foil.r*2.) + 3.*(1.+0.8*cos(length(113.1121*adjusted_uv) - foil.r*3.121))) - 1. - max(5.-length(90.*adjusted_uv), 0.), 1.), 0.);
+    vec2 rotater = vec2(cos(foil.r*0.1221), sin(foil.r*0.3512));
+    number angle = dot(rotater, adjusted_uv)/(length(rotater)*length(adjusted_uv));
+    number fac2 = max(min(5.*cos(foil.g*0.3 + angle*3.14*(2.2+0.9*sin(foil.r*1.65 + 0.2*foil.g))) - 4. - max(2.-length(20.*adjusted_uv), 0.), 1.), 0.);
+    number fac3 = 0.3*max(min(2.*sin(foil.r*5. + uv.x*3. + 3.*(1.+0.5*cos(foil.r*7.))) - 1., 1.), -1.);
+    number fac4 = 0.3*max(min(2.*sin(foil.r*6.66 + uv.y*3.8 + 3.*(1.+0.5*cos(foil.r*3.414))) - 1., 1.), -1.);
+
+    number maxfac = max(max(fac, max(fac2, max(fac3, max(fac4, 0.0)))) + 2.2*(fac+fac2+fac3+fac4), 0.);
+
+    tex.r = tex.r-delta + delta*maxfac*0.3;
+    tex.g = tex.g-delta + delta*maxfac*0.3;
+    tex.b = tex.b + delta*maxfac*1.9;
+    tex.a = min(tex.a, 0.3*tex.a + 0.9*min(0.5, maxfac*0.1));
+
+    return dissolve_mask(tex, texture_coords, uv);
+}
+
+extern MY_HIGHP_OR_MEDIUMP vec2 mouse_screen_pos;
+extern MY_HIGHP_OR_MEDIUMP float hovering;
+extern MY_HIGHP_OR_MEDIUMP float screen_scale;
+
+#ifdef VERTEX
+vec4 position( mat4 transform_projection, vec4 vertex_position )
+{
+    if (hovering <= 0.){
+        return transform_projection * vertex_position;
+    }
+    float mid_dist = length(vertex_position.xy - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy);
+    vec2 mouse_offset = (vertex_position.xy - mouse_screen_pos.xy)/screen_scale;
+    float scale = 0.2*(-0.03 - 0.3*max(0., 0.3-mid_dist))
+                *hovering*(length(mouse_offset)*length(mouse_offset))/(2. -mid_dist);
+
+    return transform_projection * vertex_position + vec4(0,0,0,scale);
+}
+#endif
