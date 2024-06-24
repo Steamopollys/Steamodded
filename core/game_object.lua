397c8940e71239d8d1347a85715a25622dd91c09
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2024-06-24 14:40:27 -0800

    Initial commit
    Co-Authored-By: Evan Wu <english5040@users.noreply.github.com>

--- core/game_object.lua
+++ core/game_object.lua
@@ -1084,6 +1084,135 @@ function loadAPIs()
     SMODS.Back:take_ownership('erratic', stake_mod('stake_orange'))
 
     -------------------------------------------------------------------------------------------------
+    ----- API CODE GameObject.Center.Booster
+    -------------------------------------------------------------------------------------------------
+
+    SMODS.Boosters = {}
+    SMODS.OPENED_BOOSTER = nil
+    SMODS.Booster = SMODS.Center:extend {
+        reverse_lookup_type = {},
+        required_params = {
+            'key',
+        },
+        prefix = 'p',
+        set = "Booster",
+        atlas = "dsix_d6_boosters",
+        type_key = "D6 Joker",
+        pos = {x = 0, y = 0},
+        loc_txt = {},
+        discovered = true,
+        weight = 1,
+        cost = 4,
+        config = {extra = 4, choose = 1},
+        inject = function(self)
+            SMODS.Center.inject(self)
+            SMODS.Boosters[self.key] = self
+        end,
+        generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
+            local vars = { self.config.choose, self.config.extra }
+            full_UI_table.name = localize{type = 'name', set = 'Other', key = self.key, nodes = full_UI_table.name}
+            localize{type = 'other', key = self.key, nodes = desc_nodes, vars = vars}
+        end,
+        open = function(self, card)
+            return create_card("Joker", G.pack_cards, nil, nil, true, true, nil, 'buf')
+        end,
+        handle_pack_uibox = function(dt)
+            local opened_booster = nil
+            for k, v in pairs(SMODS.Booster) do
+                if k == SMODS.OPENED_BOOSTER then opened_booster = v end
+            end
+            if opened_booster then opened_booster:update_pack_uibox(dt) 
+            else 
+                SMODS.Booster:update_pack_uibox(dt)
+            end
+        end,
+        update_pack_uibox = function(self, dt)
+            if G.buttons then self.buttons:remove(); G.buttons = nil end
+            if G.shop then G.shop.alignment.offset.y = G.ROOM.T.y+11 end
+        
+            if not G.STATE_COMPLETE then
+                G.STATE_COMPLETE = true
+                G.CONTROLLER.interrupt.focus = true
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'immediate',
+                    func = function()
+                        G.booster_pack = UIBox{
+                            definition = self:create_pack_uibox(),
+                            config = {align="tmi", offset = {x=0,y=G.ROOM.T.y + 9},major = G.hand, bond = 'Weak'}
+                        }
+                        G.booster_pack.alignment.offset.y = -2.2
+                                G.ROOM.jiggle = G.ROOM.jiggle + 3
+                        self:ease_background_colour()
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'immediate',
+                            func = function()
+                                G.E_MANAGER:add_event(Event({
+                                    trigger = 'after',
+                                    delay = 0.5,
+                                    func = function()
+                                        G.CONTROLLER:recall_cardarea_focus('pack_cards')
+                                        return true
+                                    end}))
+                                return true
+                            end
+                        }))  
+                        return true
+                    end
+                }))  
+            end
+        end,
+        ease_background_colour = function(self, blind_override)
+            ease_colour(G.C.DYN_UI.MAIN, G.C.FILTER)
+            ease_background_colour{new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2}
+        end,
+        create_pack_uibox = function(self)
+            local _size = self.config.extra
+            G.pack_cards = CardArea(
+              G.ROOM.T.x + 9 + G.hand.T.x, G.hand.T.y,
+              math.max(1,math.min(_size,5))*G.CARD_W*1.1,
+              1.05*G.CARD_H, 
+              {card_limit = _size, type = 'consumeable', highlight_limit = 1})
+          
+              local t = {n=G.UIT.ROOT, config = {align = 'tm', r = 0.15, colour = G.C.CLEAR, padding = 0.15}, nodes={
+                {n=G.UIT.R, config={align = "cl", colour = G.C.CLEAR,r=0.15, padding = 0.1, minh = 2, shadow = true}, nodes={
+                  {n=G.UIT.R, config={align = "cm"}, nodes={
+                  {n=G.UIT.C, config={align = "cm", padding = 0.1}, nodes={
+                    {n=G.UIT.C, config={align = "cm", r=0.2, colour = G.C.CLEAR, shadow = true}, nodes={
+                      {n=G.UIT.O, config={object = G.pack_cards}},
+                    }}
+                  }}
+                }},
+                {n=G.UIT.R, config={align = "cm"}, nodes={
+                }},
+                {n=G.UIT.R, config={align = "tm"}, nodes={
+                  {n=G.UIT.C,config={align = "tm", padding = 0.05, minw = 2.4}, nodes={}},
+                  {n=G.UIT.C,config={align = "tm", padding = 0.05}, nodes={
+                  UIBox_dyn_container({
+                    {n=G.UIT.C, config={align = "cm", padding = 0.05, minw = 4}, nodes={
+                      {n=G.UIT.R,config={align = "bm", padding = 0.05}, nodes={
+                        {n=G.UIT.O, config={object = DynaText({string = localize('k_d6_booster_pack'), colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.7, maxw = 4, pop_in = 0.5})}}
+                      }},
+                      {n=G.UIT.R,config={align = "bm", padding = 0.05}, nodes={
+                        {n=G.UIT.O, config={object = DynaText({string = {localize('k_choose')..' '}, colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.5, pop_in = 0.7})}},
+                        {n=G.UIT.O, config={object = DynaText({string = {{ref_table = G.GAME, ref_value = 'pack_choices'}}, colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.5, pop_in = 0.7})}}
+                      }},
+                    }}
+                  }),
+                }},
+                  {n=G.UIT.C,config={align = "tm", padding = 0.05, minw = 2.4}, nodes={
+                    {n=G.UIT.R,config={minh =0.2}, nodes={}},
+                    {n=G.UIT.R,config={align = "tm",padding = 0.2, minh = 1.2, minw = 1.8, r=0.15,colour = G.C.GREY, one_press = true, button = 'skip_booster', hover = true,shadow = true, func = 'can_skip_booster'}, nodes = {
+                      {n=G.UIT.T, config={text = localize('b_skip'), scale = 0.5, colour = G.C.WHITE, shadow = true, focus_args = {button = 'y', orientation = 'bm'}, func = 'set_button_pip'}}
+                    }}
+                  }}
+                }}
+              }}
+            }}
+            return t
+        end,
+    }
+
+    -------------------------------------------------------------------------------------------------
     ----- API CODE GameObject.UndiscoveredSprite
     -------------------------------------------------------------------------------------------------
 
