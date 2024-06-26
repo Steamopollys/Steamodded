a12a48243f3f351c16acabe61297ba2cf624750b
english5040 <ejwu@andrew.cmu.edu>
2024-06-26 16:49:06 -0400

    untested process_loc_text
    also comments, figuring out localization in pack_uibox

--- core/game_object.lua
+++ core/game_object.lua
@@ -1106,6 +1106,9 @@ function loadAPIs()
             SMODS.Center.inject(self)
             SMODS.Boosters[self.key] = self
         end,
+        process_loc_text = function(self)
+            SMODS.process_loc_text(G.localization.Other, self.key, self.loc_txt)
+        end,
         loc_vars = function(self, info_queue, card)
             return { vars = {card.config.choose, card.config.extra} }
         end,
@@ -1121,12 +1124,11 @@ function loadAPIs()
                 target.vars = res.vars or target.vars
                 target.key = res.key or target.key
             end
-            full_UI_table.name = localize{type = 'name', set = 'Other', key = self.key, nodes = full_UI_table.name}
+            if not full_UI_table.name then 
+                full_UI_table.name = localize{type = 'name', set = 'Other', key = self.key, nodes = full_UI_table.name}
+            end
             localize(target)
         end,
-        -- TODO generalize
-        -- TODO currently this is just supposed to create a card,
-        -- but there may be other things that open should do
         create_card = function(self, card)
             -- Example
             -- return create_card("Joker", G.pack_cards, nil, nil, true, true, nil, 'buf')
@@ -1171,50 +1173,47 @@ function loadAPIs()
             ease_colour(G.C.DYN_UI.MAIN, G.C.FILTER)
             ease_background_colour{new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2}
         end,
+        -- TODO need to replace `'TODO'` with ex. `'k_arcana_pack'`
+        -- Need to set G.localization.misc['k_arcana_pack']
         pack_uibox = function(self)
             local _size = self.config.extra
             G.pack_cards = CardArea(
-              G.ROOM.T.x + 9 + G.hand.T.x, G.hand.T.y,
-              math.max(1,math.min(_size,5))*G.CARD_W*1.1,
-              1.05*G.CARD_H, 
-              {card_limit = _size, type = 'consumeable', highlight_limit = 1})
-          
-              local t = {n=G.UIT.ROOT, config = {align = 'tm', r = 0.15, colour = G.C.CLEAR, padding = 0.15}, nodes={
+                G.ROOM.T.x + 9 + G.hand.T.x, G.hand.T.y,
+                math.max(1,math.min(_size,5))*G.CARD_W*1.1,
+                1.05*G.CARD_H, 
+                {card_limit = _size, type = 'consumeable', highlight_limit = 1})
+
+            local t = {n=G.UIT.ROOT, config = {align = 'tm', r = 0.15, colour = G.C.CLEAR, padding = 0.15}, nodes={
                 {n=G.UIT.R, config={align = "cl", colour = G.C.CLEAR,r=0.15, padding = 0.1, minh = 2, shadow = true}, nodes={
-                  {n=G.UIT.R, config={align = "cm"}, nodes={
-                  {n=G.UIT.C, config={align = "cm", padding = 0.1}, nodes={
-                    {n=G.UIT.C, config={align = "cm", r=0.2, colour = G.C.CLEAR, shadow = true}, nodes={
-                      {n=G.UIT.O, config={object = G.pack_cards}},
-                    }}
-                  }}
-                }},
-                {n=G.UIT.R, config={align = "cm"}, nodes={
-                }},
+                    {n=G.UIT.R, config={align = "cm"}, nodes={
+                    {n=G.UIT.C, config={align = "cm", padding = 0.1}, nodes={
+                        {n=G.UIT.C, config={align = "cm", r=0.2, colour = G.C.CLEAR, shadow = true}, nodes={
+                            {n=G.UIT.O, config={object = G.pack_cards}},}}}}}},
+                {n=G.UIT.R, config={align = "cm"}, nodes={}},
                 {n=G.UIT.R, config={align = "tm"}, nodes={
-                  {n=G.UIT.C,config={align = "tm", padding = 0.05, minw = 2.4}, nodes={}},
-                  {n=G.UIT.C,config={align = "tm", padding = 0.05}, nodes={
-                  UIBox_dyn_container({
-                    {n=G.UIT.C, config={align = "cm", padding = 0.05, minw = 4}, nodes={
-                      {n=G.UIT.R,config={align = "bm", padding = 0.05}, nodes={
-                        {n=G.UIT.O, config={object = DynaText({string = localize('k_d6_booster_pack'), colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.7, maxw = 4, pop_in = 0.5})}}
-                      }},
-                      {n=G.UIT.R,config={align = "bm", padding = 0.05}, nodes={
-                        {n=G.UIT.O, config={object = DynaText({string = {localize('k_choose')..' '}, colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.5, pop_in = 0.7})}},
-                        {n=G.UIT.O, config={object = DynaText({string = {{ref_table = G.GAME, ref_value = 'pack_choices'}}, colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.5, pop_in = 0.7})}}
-                      }},
-                    }}
-                  }),
-                }},
-                  {n=G.UIT.C,config={align = "tm", padding = 0.05, minw = 2.4}, nodes={
-                    {n=G.UIT.R,config={minh =0.2}, nodes={}},
-                    {n=G.UIT.R,config={align = "tm",padding = 0.2, minh = 1.2, minw = 1.8, r=0.15,colour = G.C.GREY, one_press = true, button = 'skip_booster', hover = true,shadow = true, func = 'can_skip_booster'}, nodes = {
-                      {n=G.UIT.T, config={text = localize('b_skip'), scale = 0.5, colour = G.C.WHITE, shadow = true, focus_args = {button = 'y', orientation = 'bm'}, func = 'set_button_pip'}}
-                    }}
-                  }}
-                }}
-              }}
-            }}
+                    {n=G.UIT.C,config={align = "tm", padding = 0.05, minw = 2.4}, nodes={}},
+                    {n=G.UIT.C,config={align = "tm", padding = 0.05}, nodes={
+                        UIBox_dyn_container({
+                            {n=G.UIT.C, config={align = "cm", padding = 0.05, minw = 4}, nodes={
+                                {n=G.UIT.R,config={align = "bm", padding = 0.05}, nodes={
+                                    {n=G.UIT.O, config={object = DynaText({string = localize('TODO'), colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.7, maxw = 4, pop_in = 0.5})}}}},
+                                {n=G.UIT.R,config={align = "bm", padding = 0.05}, nodes={
+                                    {n=G.UIT.O, config={object = DynaText({string = {localize('k_choose')..' '}, colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.5, pop_in = 0.7})}},
+                                    {n=G.UIT.O, config={object = DynaText({string = {{ref_table = G.GAME, ref_value = 'pack_choices'}}, colours = {G.C.WHITE},shadow = true, rotate = true, bump = true, spacing =2, scale = 0.5, pop_in = 0.7})}}}},}}
+                        }),}},
+                    {n=G.UIT.C,config={align = "tm", padding = 0.05, minw = 2.4}, nodes={
+                        {n=G.UIT.R,config={minh =0.2}, nodes={}},
+                        {n=G.UIT.R,config={align = "tm",padding = 0.2, minh = 1.2, minw = 1.8, r=0.15,colour = G.C.GREY, one_press = true, button = 'skip_booster', hover = true,shadow = true, func = 'can_skip_booster'}, nodes = {
+                            {n=G.UIT.T, config={text = localize('b_skip'), scale = 0.5, colour = G.C.WHITE, shadow = true, focus_args = {button = 'y', orientation = 'bm'}, func = 'set_button_pip'}}}}}}}}}}}}
             return t
+        end,
+    }
+
+    SMODS.Booster {
+        key = 'test_booster_pack',
+        weight = 100,
+        create_card = function(self, card)
+            return create_card("Joker", G.pack_cards, nil, nil, true, true, nil, 'buf')
         end,
     }
 
