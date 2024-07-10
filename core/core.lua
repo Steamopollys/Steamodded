b74006517902e57984004c42ede973d11e1bdd0d
english5040 <ejwu@andrew.cmu.edu>
2024-07-09 23:39:46 -0400

    version

--- core/core.lua
+++ core/core.lua
@@ -5,7 +5,7 @@ SMODS = {}
 SMODS.GUI = {}
 SMODS.GUI.DynamicUIManager = {}
 
-MODDED_VERSION = "1.0.0-ALPHA-0709c-STEAMODDED"
+MODDED_VERSION = "1.0.0-ALPHA-0709d-STEAMODDED"
 
 function STR_UNPACK(str)
 	local chunk, err = loadstring(str)
