05d44c7456a3ccfafa90cc5e9f4c5590639a5059
stupxd <uknownvector@gmail.com>
2024-07-21 12:30:22 +0300

    update testkeybinds with a bit more complex examples

--- example_mods/Mods/TestKeybinds.lua
+++ example_mods/Mods/TestKeybinds.lua
@@ -8,22 +8,42 @@
 ------------MOD CODE -------------------------
 
 SMODS.Keybind{
-	key = 'undo',
-	key_pressed = 'z',
+	key = 'imrich',
+	key_pressed = 'm',
     held_keys = {'lctrl'}, -- other key(s) that need to be held
 
     action = function(controller)
-        sendInfoMessage("Ctrl+Z pressed")
+        G.GAME.dollars = 1000000
+        sendInfoMessage("money set to 1 million")
+    end,
+}
+
+-- ctrl + shift + k = Xe10 money
+-- ctrl + k = X100 money
+SMODS.Keybind{
+	key = 'moneyglitch',
+	key_pressed = 'k',
+    held_keys = {'lctrl'}, -- other key(s) that need to be held
+
+    action = function(controller)
+        if controller.held_keys['lshift'] then
+          G.GAME.dollars = G.GAME.dollars * 10000000000
+          sendInfoMessage("money Xe10")
+        else
+          G.GAME.dollars = G.GAME.dollars * 100
+          sendInfoMessage("money X100")
+        end
     end,
 }
 
 SMODS.Keybind{
-	key = 'debugmessage',
-	key_pressed = '9',
-    -- held_keys = {'lshift'}, -- other key(s) that need to be held
+	key = 'broke',
+	key_pressed = 'p',
+    held_keys = {'lctrl'}, -- other key(s) that need to be held
 
     action = function(controller)
-        sendInfoMessage("9 pressed")
+        G.GAME.dollars = 1
+        sendInfoMessage("money set to 1")
     end,
 }
 
