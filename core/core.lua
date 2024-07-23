a5109d133081d85e870486cae16f576240c5f2f1
Casimir Eisenach <casimirusenormus@hotmail.de>
2024-07-24 01:28:22 +0200

    version

--- core/core.lua
+++ core/core.lua
@@ -2,7 +2,7 @@
 --- MODULE CORE
 
 SMODS = {}
-MODDED_VERSION = "1.0.0-ALPHA-0723c-STEAMODDED"
+MODDED_VERSION = "1.0.0-ALPHA-0724a-STEAMODDED"
 
 -- Include lovely and nativefs modules
 local nativefs = require "nativefs"
