347ef602bffbc590c1f235be50ce8b34b020cb36
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2024-07-29 11:01:08 -0800

    Unscuff UI

--- core/ui.lua
+++ core/ui.lua
@@ -148,7 +148,6 @@ function create_UIBox_mods(args)
 			chosen = SMODS.LAST_SELECTED_MOD_TAB == "achievements" or false,
 			tab_definition_function = function()
 				SMODS.LAST_SELECTED_MOD_TAB = "achievements"
-				G.SMODS_ACHIEVEMENTS_MATRIX = nil
 				return buildAchievementsTab(mod)
 			end
 		})
@@ -495,167 +494,16 @@ G.UIDEF.consumable_collection_page = function(page)
 	return t
 end
 
-function buildAchievementsTab(mod, args)
-	args = args or {}
+function buildAchievementsTab(mod, current_page)
+	current_page = current_page or 1
 	fetch_achievements()
+	local achievement_matrix = {{},{}}
 	local achievements_per_row = 4
 	local achievements_pool = {}
 	for k, v in pairs(G.ACHIEVEMENTS) do
 		if v.mod and v.mod.id == mod.id then achievements_pool[#achievements_pool+1] = v end
 	end
-	if not G.SMODS_ACHIEVEMENTS_MATRIX then
-		G.SMODS_ACHIEVEMENTS_MATRIX = {{},{}}
-		local achievement_tab = {}
-		for k, v in pairs(achievements_pool) do
-			achievement_tab[#achievement_tab+1] = v
-		end
-		
-		table.sort(achievement_tab, function(a, b) return (a.order or 1) < (b.order or 1) end)
-		
-		local row = 1
-		for i = 1, achievements_per_row*2 do
-			local v = achievement_tab[i]
-			local temp_achievement = Sprite(0,0,1.25,1.25,G.ASSET_ATLAS[v.atlas or "achievements"], v.earned and v.pos or {x=0, y=0})
-			temp_achievement:define_draw_steps({
-				{shader = 'dissolve', shadow_height = 0.05},
-				{shader = 'dissolve'}
-			})
-			if i == 1 then 
-				G.E_MANAGER:add_event(Event({
-				trigger = 'immediate',
-				func = (function()
-					G.CONTROLLER:snap_to{node = temp_achievement}
-					return true
-				end)
-				}))
-			end
-			temp_achievement.float = true
-			temp_achievement.states.hover.can = true
-			temp_achievement.states.drag.can = false
-			temp_achievement.states.collide.can = true
-			--temp_achievement.config = {blind = v, force_focus = true}
-			temp_achievement.hover = function()
-				if not G.CONTROLLER.dragging.target or G.CONTROLLER.using_touch then 
-					if not temp_achievement.hovering and temp_achievement.states.visible then
-						temp_achievement.hovering = true
-						temp_achievement.hover_tilt = 3
-						temp_achievement:juice_up(0.05, 0.02)
-						play_sound('chips1', math.random()*0.1 + 0.55, 0.12)
-						Node.hover(temp_achievement)
-						if temp_achievement.children.alert then 
-							temp_achievement.children.alert:remove()
-							temp_achievement.children.alert = nil
-							v.alerted = true
-							G:save_progress()
-						end
-					end
-				end
-				temp_achievement.stop_hover = function() temp_achievement.hovering = false; Node.stop_hover(temp_achievement); temp_achievement.hover_tilt = 0 end
-			end
 
-			-- Description
-			local achievement_text = {}
-			local maxCharsPerLine = 30
-			local function wrapText(text, maxChars)
-				local wrappedText = {""}
-				local curr_line = 1
-				local currentLineLength = 0
-			
-				for word in text:gmatch("%S+") do
-					if currentLineLength + #word <= maxChars then
-						wrappedText[curr_line] = wrappedText[curr_line] .. word .. ' '
-						currentLineLength = currentLineLength + #word + 1
-					else
-						wrappedText[curr_line] = string.sub(wrappedText[curr_line], 0, -2)
-						curr_line = curr_line + 1
-						wrappedText[curr_line] = ""
-						wrappedText[curr_line] = wrappedText[curr_line] .. word .. ' '
-						currentLineLength = #word + 1
-					end
-				end
-			
-				wrappedText[curr_line] = string.sub(wrappedText[curr_line], 0, -2)
-				return wrappedText
-			end
-		
-			local loc_target = (v.hidden_text and not v.earned) and {localize("hidden_achievement", 'achievement_descriptions')} or wrapText(localize(v.key, 'achievement_descriptions'), maxCharsPerLine)
-			local loc_name = (v.hidden_name and not v.earned) and localize("hidden_achievement", 'achievement_names') or localize(v.key, 'achievement_names')
-
-			local ability_text = {}
-			if loc_target then 
-				for k, v in ipairs(loc_target) do
-					ability_text[#ability_text + 1] = {n=G.UIT.R, config={align = "cm"}, nodes={{n=G.UIT.T, config={text = v, scale = 0.35, shadow = true, colour = G.C.WHITE}}}}
-				end
-			end
-			achievement_text[#achievement_text + 1] =
-			{n=G.UIT.R, config={align = "cm", emboss = 0.05, r = 0.1, minw = 2.5, padding = 0.07, colour = G.C.WHITE}, nodes={
-				ability_text[1] and {n=G.UIT.R, config={align = "cm", padding = 0.08, colour = G.C.GREY, r = 0.1, emboss = 0.05, minw = 2.5, minh = 0.9}, nodes=ability_text} or nil
-			}}
-
-			table.insert(G.SMODS_ACHIEVEMENTS_MATRIX[row], {
-				n = G.UIT.C,
-				config = { align = "cm", padding = 0.1 },
-				nodes = {
-					{n=G.UIT.R, config = {align = "cm"}, nodes = {
-						{n=G.UIT.R, config = {align = "cm", padding = 0.1}, nodes = {{ n = G.UIT.O, config = { object = temp_achievement, focus_with_object = true }}}},
-						{
-							n=G.UIT.R, config = {align = "cm", minw = 2.5, padding = 0.1, colour = G.C.GREY}, nodes = {
-								{n=G.UIT.R, config={align = "cm", emboss = 0.05, r = 0.1, minw = 2.5, padding = 0.1, colour = G.C.GREY}, nodes={
-									{n=G.UIT.O, config={align = "cm", object = DynaText({string = loc_name, colours = {G.C.UI.TEXT_LIGHT}, shadow = true, spacing = 1, bump = true, scale = 0.4})}},
-								}},
-								{n=G.UIT.R, config={align = "cm"}, nodes=achievement_text},
-							},
-						},
-					}},
-				},
-			})
-			if #G.SMODS_ACHIEVEMENTS_MATRIX[row] == achievements_per_row then 
-				row = row + 1
-				G.SMODS_ACHIEVEMENTS_MATRIX[row] = {}
-			end
-		end
-	end
-
-	local achievements_options = {}
-	for i = 1, math.ceil(#achievements_pool/(2*achievements_per_row)) do
-		table.insert(achievements_options, localize('k_page')..' '..tostring(i)..'/'..tostring(math.ceil(#achievements_pool/(2*achievements_per_row))))
-	end
-
-	local t = {
-		{n=G.UIT.C, config={}, nodes={ 
-		{n=G.UIT.C, config={align = "cm"}, nodes={
-		{n=G.UIT.R, config={align = "cm"}, nodes={
-			{n=G.UIT.R, config={align = "cm"}, nodes=G.SMODS_ACHIEVEMENTS_MATRIX[1]},
-			{n=G.UIT.R, config={align = "cm"}, nodes=G.SMODS_ACHIEVEMENTS_MATRIX[2]},
-			create_option_cycle({options = achievements_options, w = 4.5, cycle_shoulders = true, opt_callback = 'achievments_tab_page', focus_args = {snap_to = true, nav = 'wide'},current_option = args.selected_tab or 1, colour = G.C.RED, no_pips = true})
-		}}
-		}}
-	}}}
-	return {
-		n = G.UIT.ROOT,
-		config = {
-			emboss = 0.05,
-			minh = 6,
-			r = 0.1,
-			minw = 6,
-			align = "tm",
-			padding = 0.2,
-			colour = G.C.BLACK
-		},
-		nodes = t
-	}
-end
-
-G.FUNCS.achievments_tab_page = function(args)
-	if not args or not args.cycle_config then return end
-	G.SMODS_ACHIEVEMENTS_MATRIX = {{},{}}
-	
-	fetch_achievements()
-	local achievements_per_row = 4
-	local achievements_pool = {}
-	for k, v in pairs(G.ACHIEVEMENTS) do
-		if v.mod and v.mod.id == G.ACTIVE_MOD_UI.id then achievements_pool[#achievements_pool+1] = v end
-	end
 	local achievement_tab = {}
 	for k, v in pairs(achievements_pool) do
 		achievement_tab[#achievement_tab+1] = v
@@ -665,7 +513,7 @@ G.FUNCS.achievments_tab_page = function(args)
 	
 	local row = 1
 	for i = 1, achievements_per_row*2 do
-		local v = achievement_tab[i+(8*(args.cycle_config.current_option - 1))]
+		local v = achievement_tab[i+((achievements_per_row*2)*(current_page-1))]
 		if not v then break end
 		local temp_achievement = Sprite(0,0,1.25,1.25,G.ASSET_ATLAS[v.atlas or "achievements"], v.earned and v.pos or {x=0, y=0})
 		temp_achievement:define_draw_steps({
@@ -744,14 +592,14 @@ G.FUNCS.achievments_tab_page = function(args)
 			ability_text[1] and {n=G.UIT.R, config={align = "cm", padding = 0.08, colour = G.C.GREY, r = 0.1, emboss = 0.05, minw = 2.5, minh = 0.9}, nodes=ability_text} or nil
 		}}
 
-		table.insert(G.SMODS_ACHIEVEMENTS_MATRIX[row], {
+		table.insert(achievement_matrix[row], {
 			n = G.UIT.C,
 			config = { align = "cm", padding = 0.1 },
 			nodes = {
 				{n=G.UIT.R, config = {align = "cm"}, nodes = {
 					{n=G.UIT.R, config = {align = "cm", padding = 0.1}, nodes = {{ n = G.UIT.O, config = { object = temp_achievement, focus_with_object = true }}}},
 					{
-						n=G.UIT.R, config = {align = "cm", minw = 2.5, padding = 0.1, colour = G.C.GREY}, nodes = {
+						n=G.UIT.R, config = {align = "cm"}, nodes = {
 							{n=G.UIT.R, config={align = "cm", emboss = 0.05, r = 0.1, minw = 2.5, padding = 0.1, colour = G.C.GREY}, nodes={
 								{n=G.UIT.O, config={align = "cm", object = DynaText({string = loc_name, colours = {G.C.UI.TEXT_LIGHT}, shadow = true, spacing = 1, bump = true, scale = 0.4})}},
 							}},
@@ -761,16 +609,50 @@ G.FUNCS.achievments_tab_page = function(args)
 				}},
 			},
 		})
-		if #G.SMODS_ACHIEVEMENTS_MATRIX[row] == achievements_per_row then 
+		if #achievement_matrix[row] == achievements_per_row then 
 			row = row + 1
-			G.SMODS_ACHIEVEMENTS_MATRIX[row] = {}
+			achievement_matrix[row] = {}
 		end
 	end
+
+	local achievements_options = {}
+	for i = 1, math.ceil(#achievements_pool/(2*achievements_per_row)) do
+		table.insert(achievements_options, localize('k_page')..' '..tostring(i)..'/'..tostring(math.ceil(#achievements_pool/(2*achievements_per_row))))
+	end
+
+	local t = {
+		{n=G.UIT.C, config={}, nodes={ 
+		{n=G.UIT.C, config={align = "cm"}, nodes={
+		{n=G.UIT.R, config={align = "cm"}, nodes={
+			{n=G.UIT.R, config={align = "cm"}, nodes=achievement_matrix[1]},
+			{n=G.UIT.R, config={align = "cm"}, nodes=achievement_matrix[2]},
+			create_option_cycle({options = achievements_options, w = 4.5, cycle_shoulders = true, opt_callback = 'achievments_tab_page', focus_args = {snap_to = true, nav = 'wide'},current_option = current_page, colour = G.C.RED, no_pips = true})
+		}}
+		}}
+	}}}
+	return {
+		n = G.UIT.ROOT,
+		config = {
+			emboss = 0.05,
+			minh = 6,
+			r = 0.1,
+			minw = 6,
+			align = "tm",
+			padding = 0.2,
+			colour = G.C.BLACK
+		},
+		nodes = t
+	}
+end
+
+G.FUNCS.achievments_tab_page = function(args)
+	if not args or not args.cycle_config then return end
+	achievement_matrix = {{},{}}
 
 	local tab_contents = G.OVERLAY_MENU:get_UIE_by_ID('tab_contents')
 	tab_contents.config.object:remove()
 	tab_contents.config.object = UIBox{
-		definition = buildAchievementsTab(G.ACTIVE_MOD_UI, {selected_tab = args.cycle_config.current_option}),
+		definition = buildAchievementsTab(G.ACTIVE_MOD_UI, args.cycle_config.current_option),
 		config = {offset = {x=0,y=0}, parent = tab_contents, type = 'cm'}
 	}
 	tab_contents.UIBox:recalculate()
