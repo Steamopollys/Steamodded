44479c0ee4b7753ef73a656cde51a0ffefe58fbf
larswijn <51133685+larswijn@users.noreply.github.com>
2024-08-01 20:14:09 +0200

    Fix space issue in path when restarting

--- core/utils.lua
+++ core/utils.lua
@@ -320,7 +320,7 @@ end
 
 function SMODS.restart_game()
     if love.system.getOS() ~= 'OS X' then
-        love.thread.newThread("os.execute(...)\n"):start(arg[-2] .. " " .. table.concat(arg, " "))
+        love.thread.newThread("os.execute(...)\n"):start('"' .. arg[-2] .. '" ' .. table.concat(arg, " "))
     else
         os.execute('sh "/Users/$USER/Library/Application Support/Steam/steamapps/common/Balatro/run_lovely.sh" &')
     end
