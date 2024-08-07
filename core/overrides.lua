3c61ffc9ca617ad473dc955d31a4804246912243
MathIsFun0 <89877664+MathIsFun0@users.noreply.github.com>
2024-08-06 22:32:52 -0400

    refactor to use only context variable

--- core/overrides.lua
+++ core/overrides.lua
@@ -1229,8 +1229,9 @@ end
 --#endregion
 --#region joker retrigger API
 local cj = Card.calculate_joker
-function Card:calculate_joker(context, callback, retrigger, no_retrigger_anim)
-    local ret, triggered = cj(self, context, callback, retrigger, no_retrigger_anim)
+function Card:calculate_joker(context)
+    --jank but fixes bugs
+	local ret, triggered = cj(self, context)
     --Check for retrggering jokers
     if (ret or triggered) and context and not context.retrigger_joker and not context.retrigger_joker_check then
         if type(ret) ~= 'table' then ret = {joker_repetitions = {0}} end
@@ -1256,96 +1257,17 @@ function Card:calculate_joker(context, callback, retrigger, no_retrigger_anim)
             if type(ret.joker_repetitions[z]) == 'table' and ret.joker_repetitions[z].repetitions then
                 for r = 1, ret.joker_repetitions[z].repetitions do
                 if percent then percent = percent+percent_delta end
-                context.retrigger_joker = ret.joker_repetitions[z].card
-                local _ret, _triggered = self:calculate_joker(context, callback, ret.joker_repetitions[z])
-                if (_ret or _triggered) and not no_retrigger_anim then card_eval_status_text(ret.joker_repetitions[z].card, 'jokers', nil, nil, nil, ret.joker_repetitions[z]) end
+                context.retrigger_joker = ret.joker_repetitions[z]
+                local _ret, _triggered = self:calculate_joker(context, callback)
+                if (_ret or _triggered) and not context.no_retrigger_anim then card_eval_status_text(ret.joker_repetitions[z].card, 'jokers', nil, nil, nil, ret.joker_repetitions[z]) end
                 end
             end
         end
     end
-    if callback and type(callback) == 'function' then callback(ret, retrigger) end
+    if context.callback and type(context.callback) == 'function' then context.callback(ret, context.retrigger_joker) end
     return ret, triggered
 end    
-local calc_jkr = Card.calculate_joker --for some reason the normal way isn't working
 function Card:calculate_joker_retriggers()
     return
-end
-function eval_card(card, context, callback)
-    context = context or {}
-    local ret = {}
-
-    if context.repetition_only then
-        local seals = card:calculate_seal(context)
-        if seals then
-            ret.seals = seals
-        end
-        return ret
-    end
-    
-    if context.cardarea == G.play then
-        local chips = card:get_chip_bonus()
-        if chips > 0 then 
-            ret.chips = chips
-        end
-
-        local mult = card:get_chip_mult()
-        if mult > 0 then 
-            ret.mult = mult
-        end
-
-        local x_mult = card:get_chip_x_mult(context)
-        if x_mult > 0 then 
-            ret.x_mult = x_mult
-        end
-
-        local p_dollars = card:get_p_dollars()
-        if p_dollars > 0 then 
-            ret.p_dollars = p_dollars
-        end
-
-        local jokers = calc_jkr(card, context, callback)
-        if jokers then 
-            ret.jokers = jokers
-        end
-
-        local edition = card:get_edition(context)
-        if edition then 
-            ret.edition = edition
-        end
-    end
-
-    if context.cardarea == G.hand then
-        local h_mult = card:get_chip_h_mult()
-        if h_mult > 0 then 
-            ret.h_mult = h_mult
-        end
-
-        local h_x_mult = card:get_chip_h_x_mult()
-        if h_x_mult > 0 then 
-            ret.x_mult = h_x_mult
-        end
-
-        local jokers = calc_jkr(card, context, callback)
-        if jokers then 
-            ret.jokers = jokers
-        end
-    end
-
-    if context.cardarea == G.jokers or context.card == G.consumeables then
-        local jokers = nil
-        if context.edition then
-            jokers = card:get_edition(context)
-        elseif context.other_joker then
-            jokers = calc_jkr(context.other_joker, context, callback)
-        else
-			print(type(callback))
-            jokers = calc_jkr(card, context, callback)
-        end
-        if jokers then 
-            ret.jokers = jokers
-        end
-    end
-
-    return ret
 end
 --#endregion
