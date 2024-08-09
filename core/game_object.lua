7e37b168f964d69f932b27a60350910fec3bca3d
Eremel <richardbailey@outlook.com>
2024-08-10 00:35:45 +0100

    prelimenary blind amount scaling past base game values

--- core/game_object.lua
+++ core/game_object.lua
@@ -610,6 +610,26 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
         end
     end
 
+    function SMODS.get_blind_amount(ante)
+        local scale = G.GAME.modifiers.scaling
+        local amounts = {
+            300,
+            700 + 100*scale,
+            1400 + 600*scale,
+            2100 + 2900*scale,
+            15000 + 5000*scale,
+            12000 + 8000*(scale+1)*(scale/2),
+            10000 + 25000*(2 ^ (scale-1)),
+            50000 * (2 ^ (scale-1))
+        }
+        if ante < 1 then return 100 end
+        if ante <= 8 then return amounts[ante] end
+        local a, c, d = amounts[8], ante-8, 1 + 0.2*(ante-8)
+        local amount = math.floor(a*(1.6+(0.75*c)^d)^c)
+        amount = amount - amount%(10^math.floor(math.log10(amount)-1))
+        return amount
+    end
+
     --Register vanilla stakes
     G.P_STAKES = {}
     SMODS.Stake {
